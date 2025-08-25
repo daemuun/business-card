43aafac2590c9f5e8886fb3fef312e9e097d4326
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2025-08-25 12:29:50 +0300

    feat: Add vertical scroll to description column

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Call
 import androidx.compose.material.icons.rounded.Email
@@ -120,7 +122,7 @@ fun CardDescription(
     description: String,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.verticalScroll(state = rememberScrollState())) {
         Text(
             text = title,
             textAlign = TextAlign.Center,
